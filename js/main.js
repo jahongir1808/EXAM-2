f6303f489e527ede2f53525458a26c107e5ede56
jahongir1808 <jahongir.developer18@gmail.com>
2023-01-21 16:08:20 +0500

    loading added

--- js/main.js
+++ js/main.js
@@ -65,3 +65,11 @@ function shrink() {
     navbar.classList.remove("navbar-shrink");
   }
 }
+
+window.addEventListener("load", () => {
+  document.getElementById("loading").style.display = "none";
+});
+// or
+setTimeout(() => {
+  document.getElementById("loading").style.display = "none";
+}, 2000);
